489ae1527c9795d8b65510989a02f601ad1ef1a5
jayesh <jayeshsingh0803@gmail.com>
2023-07-26 10:12:01 +0530

    Completed ECommerce

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+    {
+      _id: 0,
+      name: "Rs0 to 199",
+      array: [0, 199],
+    },
+    {
+      _id: 1,
+      name: "Rs200 to 399",
+      array: [200, 399],
+    },
+    {
+      _id: 2,
+      name: "Rs400 to 599",
+      array: [400, 599],
+    },
+    {
+      _id: 3,
+      name: "Rs600 to 799",
+      array: [600, 799],
+    },
+    {
+      _id: 4,
+      name: "Rs800 to 999",
+      array: [800, 999],
+    },
+    {
+      _id: 4,
+      name: "Rs1000 or more",
+      array: [1000, 9999],
+    },
+  ];
--- server.js
+++ server.js
@@ -1,35 +1,44 @@
 import express from "express";
-import dotenv from "dotenv";
 import colors from "colors";
-import connectDB from "./config/db.js";
-import authRoutes from './routes/authRoute.js'
+import dotenv from "dotenv";
 import morgan from "morgan";
-const app = express();
-
-//database
-connectDB();
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
 
 //configure env
 dotenv.config();
 
-//middleware
+//databse config
+connectDB();
+
+//rest object
+const app = express();
+
+//middelwares
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use('/api/v1/auth', authRoutes);
+//routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
+//rest api
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to ecommerce app</h1>");
+});
 
-app.get('/', (req,res)=>
-{
-    res.send({
-        message:"Welcome to the ECommerce app"
-    })
-})
+//PORT
+const PORT = process.env.PORT || 8000;
 
-//PORT FOR SERVER
-const PORT = process.env.PORT || 8000
- 
-app.listen(PORT, function()
-{
-    console.log(`Server running on mode: ${process.env.DEV_MODE} and port: ${PORT}`.bgRed.white);
-})
+//run listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
+});
